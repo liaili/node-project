95cac20a93b33542e1896b3d9db2f12fdbd07280
wb371710 <wb-lal371710@alibaba-inc.com>
2020-06-18 16:30:57 +0800

    feat: add a game

--- chapter1/index.js
+++ chapter1/index.js
@@ -1,1 +1,24 @@
-console.log('hello world');
+var playerAction = process.argv[process.argv.length - 1];
+console.log('playerAction', playerAction);
+
+var random = Math.random() * 3;
+
+if (random < 1) {
+  var computerAction = 'rock';
+} else if (random > 2) {
+  var computerAction = 'scissor';
+} else {
+  var computerAction = 'paper';
+}
+console.log('computerAction', computerAction);
+if (computerAction === playerAction) {
+  console.log('平局');
+} else if(
+  (playerAction === 'rock' && computerAction === 'scissor') ||
+  (playerAction === 'scissor' && computerAction === 'paper') ||
+  (playerAction === 'paper' && computerAction === 'rock')
+) {
+  console.log('你赢了');
+} else {
+  console.log('你输了');
+}
