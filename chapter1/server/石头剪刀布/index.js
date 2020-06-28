63eebe6acb29c27041bdb3d6e24c61aecdd58276
wb371710 <wb-lal371710@alibaba-inc.com>
2020-06-28 14:42:13 +0800

    express

--- chapter1/server/石头剪刀布/index.js
+++ chapter1/server/石头剪刀布/index.js
@@ -2,46 +2,68 @@ const querystring = require('querystring');
 const http = require('http');
 const url = require('url');
 const fs = require('fs');
+const express = require('express');
+
 const game = require('./game');
 
 let playwon = 0;
+let playerLastAction = '';
+let sameCount = 0;
 
-http
-  .createServer((req, res) => {
-    const parseurl = url.parse(req.url);
+const app = express();
 
-    if (parseurl.pathname === '/favicon.ico') {
-      res.writeHead(200);
-      res.end();
-      return;
-     }
+// app.use(function(req, res){
+  
+// });
 
-     if (parseurl.pathname === '/game') {
-       const query = querystring.parse(parseurl.query);
-       const playerAction = query.action;
+app.get('/favicon.ico', function(req, res) {
+  res.writeHead(200);
+  res.end();
+  return;
+})
 
-      const gameResult = game(playerAction);
+app.get('/game', function(req, res) {
+  const parseurl = querystring.parse(req.url);
+  const query = querystring.parse(parseurl.query);
+  const playerAction = query.action;
 
-      if (playwon >= 3) {
-        res.writeHead(500);
-        res.end('我再也不和你玩了');
-      }
+  const gameResult = game(playerAction);
 
-      res.writeHead(200);
-      if (gameResult === 0) {
-        res.end('平局');
-      } else if (gameResult === 1) {
-        res.end('你赢了');
-        playwon++;
-      } else {
-        res.end('你输了');
-      }
-     }
+  if (playwon >= 3 || sameCount === 9) {
+    res.writeHead(500);
+    res.end('我再也不和你玩了');
+  }
 
-     if (parseurl.pathname === '/') {
-       res.writeHead(200);
-       fs.createReadStream(__dirname + '/index.html')
-        .pipe(res);
-     }
-  })
-  .listen(8080)
+  if (playerAction && playerLastAction === playerAction) {
+    sameCount++;
+  } else {
+    sameCount = 0;
+  }
+  playerLastAction = playerAction;
+
+  if (sameCount >= 3) {
+    res.writeHead(400);
+    res.end('你作弊了');
+    sameCount = 9;
+  }
+
+  res.writeHead(200);
+  if (gameResult === 0) {
+    res.end('平局');
+  } else if (gameResult === 1) {
+    res.end('你赢了');
+    playwon++;
+  } else {
+    res.end('你输了');
+  }
+})
+
+app.get('/', function(req, res) {
+  console.log('/')
+  res.writeHead(200);
+  fs.createReadStream(__dirname + '/index.html')
+  .pipe(res);
+})
+
+app.listen(8080)
+
