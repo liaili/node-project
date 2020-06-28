1e0fa1068f856b3ddbefdf422a1ac46c8a073483
wb371710 <wb-lal371710@alibaba-inc.com>
2020-06-28 11:22:56 +0800

    feat: server2

--- chapter1/server/index.js
+++ chapter1/server/index.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const fs = require('fs');
 
  http.createServer((req, res) => {
    if (req.url === '/favicon.ico') {
@@ -7,8 +8,8 @@ const http = require('http');
     return;
    }
 
-   console.log(req.url);
    res.writeHead(200);
-   res.end('hello');
+   fs.createReadStream(__dirname + '/index.html')
+   .pipe(res);
  })
  .listen(3000)
